100635cffcd03ed4cb3a22aa466eb4d606447290
Claudionei de Aguiar <cdaguiar23@gmail.com>
2025-10-17 17:58:20 -0300

    feat: configura roteamento da aplicação com Vue Router
    - Cria router/index.js com todas as rotas da aplicação
    - Implementa proteção de rotas autenticadas
    - Define rotas para: login, listagem, criação, edição e detalhes de produtos
    - Adiciona guards de navegação para redirecionamento automático

---
+++ src/router/index.js
@@ -0,0 +1,61 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '../stores/auth'
+
+import LoginView from '../views/LoginView.vue'
+import ProdutoListView from '../views/ProdutoListView.vue'
+import ProdutoFormView from '../views/ProdutoFormView.vue'
+import ProdutoDetailView from '../views/ProdutoDetailView.vue'
+
+const routes = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginView,
+    meta: { requiresAuth: false }
+  },
+  {
+    path: '/',
+    name: 'ProdutoList',
+    component: ProdutoListView,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/produtos/novo',
+    name: 'ProdutoCreate',
+    component: ProdutoFormView,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/produtos/:id/editar',
+    name: 'ProdutoEdit',
+    component: ProdutoFormView,
+    meta: { requiresAuth: true },
+    props: true
+  },
+  {
+    path: '/produtos/:id',
+    name: 'ProdutoDetail',
+    component: ProdutoDetailView,
+    meta: { requiresAuth: true },
+    props: true
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+
+  if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+    next('/login')
+  } else if (to.name === 'Login' && authStore.isAuthenticated) {
+    next('/')
+  } else {
+    next()
+  }
+})
+
+export default router
